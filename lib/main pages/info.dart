35de5ad5ff1cdb06878167abcec2debe61f63e7d
Brandon-F23 <Brandon.flores03@utrgv.edu>
2023-03-22 16:35:13 -0500

    Added buttons on information page for our sources so far

--- lib/main pages/info.dart
+++ lib/main pages/info.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri _url = Uri.parse('https://us.openfoodfacts.org/nutriscore');
+final Uri _url2 = Uri.parse('https://us.openfoodfacts.org/');
 
 class InfoPage extends StatefulWidget {
   const InfoPage({super.key});
@@ -34,9 +38,35 @@ class _InfoPageState extends State<InfoPage> {
               'Information',
               style: GoogleFonts.bebasNeue(color: Colors.black, fontSize: 45),
             ),
-          )
+          ),
+          SliverToBoxAdapter(
+              child: Column(
+            children: [
+              Text('Our sources'),
+              ElevatedButton(
+                onPressed: _launchUrl,
+                child: Text('NutriScore'),
+              ),
+              ElevatedButton(
+                onPressed: _launchUrl2,
+                child: Text('Open Food Facts'),
+              ),
+            ],
+          )),
         ],
       ),
     );
+  }
+}
+
+Future<void> _launchUrl() async {
+  if (!await launchUrl(_url)) {
+    throw Exception('Could not launch $_url');
+  }
+}
+
+Future<void> _launchUrl2() async {
+  if (!await launchUrl(_url2)) {
+    throw Exception('Could not launch $_url');
   }
 }
